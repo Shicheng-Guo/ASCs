d84b784991639fddcc14c0c779635289c0240971
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-07-31 22:31:35 -0500

    Create hisat2.sh

---
+++ hisat2.sh
@@ -0,0 +1,7 @@
+cp 1st/* vdj/
+cp 2nd/* vdj/
+gunzip *.gz 
+http://ccb.jhu.edu/software/hisat2/dl/hisat2-2.1.0-source.zip
+unzip hisat2-2.1.0-source.zip
+cd /gpfs/home/guosa/hpc/tools/hisat2-2.1.0
+make
