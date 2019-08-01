600b046a7fac53a0c6b904cd270b4e7e8460c77d
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-08-01 00:46:05 -0500

    Update hisat2.sh

--- hisat2.sh
+++ hisat2.sh
@@ -5,3 +5,8 @@ http://ccb.jhu.edu/software/hisat2/dl/hisat2-2.1.0-source.zip
 unzip hisat2-2.1.0-source.zip
 cd /gpfs/home/guosa/hpc/tools/hisat2-2.1.0
 make
+
+mkdir indexes
+cd indexes
+wget ftp://ftp.ccb.jhu.edu/pub/infphilo/hisat2/data/grch38_tran.tar.gz
+
